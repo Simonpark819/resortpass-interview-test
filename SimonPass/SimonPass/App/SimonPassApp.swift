66aa6af57652543c201a0dc4c482fde4bcb71cad
Simon Park <simonpark819@gmail.com>
2026-06-03 13:43:07 -0400

    [bootstrap] create proj and setup initial scaffolding and structure

---
+++ SimonPass/SimonPass/App/SimonPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonPassApp.swift
+//  SimonPass
+//
+//  Created by Simon YoungHoon Park on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SimonPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
